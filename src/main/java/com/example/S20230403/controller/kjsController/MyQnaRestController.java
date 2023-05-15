45f95fb9ece0797e2f110f7f228d3edab553b8a9
user <user@DESKTOP-2PGRMF4>
2023-05-15 09:18:38 +0900

    2023-05-15 09:18 건화 푸쉬 브랜치

--- src/main/java/com/example/S20230403/controller/kjsController/MyQnaRestController.java
+++ src/main/java/com/example/S20230403/controller/kjsController/MyQnaRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.S20230403.auth.PrincipalDetail;
@@ -17,14 +18,32 @@ import lombok.RequiredArgsConstructor;
 public class MyQnaRestController {
 	private final MyQnaService ms;
 	
-	@PostMapping("/commonUser/myQna")
+	//qna 리스트
+//	@PostMapping("/commonUser/myQna")
+	@RequestMapping("/commonUser/myQna")
 	public List<Qna> myQna(@AuthenticationPrincipal PrincipalDetail userDetail, 
 										String qna_type) {
 		
 		String user_id = userDetail.getUsername();
-		
 		List<Qna> myQna = ms.getMyQnaList(user_id, qna_type);
 		
 		return myQna;
 	}
+	
+	//qna 수정
+	@PostMapping("/commonUser/myQnaUpdate")
+	public void myQnaUpdate(Qna qna) {
+		System.out.println("qna업데이트 qnaTitle->"+ qna.getQna_title());
+		System.out.println("qna업데이트 qnaContent->"+ qna.getQna_content());
+		System.out.println("qna업데이트 qna_id->" + qna.getQna_id());
+		ms.myQnaUpdate(qna);
+	}
+	
+	//qna 삭제
+	@PostMapping("/commonUser/myQnaDelete")
+	public void myQnaDelete(int qna_id) {
+		System.out.println("qna_id--->" + qna_id);
+		ms.myQnaDelete(qna_id);
+	}
+	
 }
