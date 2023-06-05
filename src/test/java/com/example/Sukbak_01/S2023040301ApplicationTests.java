da29d35ad99a1cdb21afc1cf05e8a87d17c5d5f0
qwezxc333 <qwezxc334@naver.com>
2023-06-05 22:03:18 +0900

    sukbak kjs 6/5 commit

---
+++ src/test/java/com/example/Sukbak_01/S2023040301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sukbak_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sukbak01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
