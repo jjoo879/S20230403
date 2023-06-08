596d53b241bd96869f95f036a2f77e12e114ecf7
User <User@DESKTOP-JFUABTI>
2023-06-08 10:54:33 +0900

    2023.6.8_테이블 색상변경

--- src/main/java/com/example/S20230403/controller/lcgController/ProductsFilterController.java
+++ src/main/java/com/example/S20230403/controller/lcgController/ProductsFilterController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.S20230403.auth.PrincipalDetail;
@@ -16,21 +17,25 @@ import com.example.S20230403.service.lcgService.ProductsFilterService;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ProductsFilterController {
 	private final ProductsFilterService service;
 	
 
 	// 1.accom_type별로 숙소 리스트를 가져오기 위한 로직
-	@GetMapping("/cgProductLists")
-	public String cgGetProductByAccomtype(@AuthenticationPrincipal PrincipalDetail userDetail,
-										  @RequestParam("accom-type") String accom_type, 
-										  Accom accom, 
-										  Model model) {
-//		System.out.println("리펙토링 cgProductByAccomtype 시작");
-//		System.out.println("리펙토링 cgProductByAccomtype accom_type->"+accom_type);
+	@GetMapping("/cgProductLists/accom-type/{accom-type}")
+	public String cgProductListByAccomtype(@AuthenticationPrincipal PrincipalDetail userDetail,
+										   @PathVariable("accom-type") String accom_type, 
+										   Accom accom, 
+										   Model model) {
+		
+		log.info("리펙토링 cgProductByAccomtype 시작");
+		log.info("리펙토링 cgProductByAccomtype accom_type-> {} ", accom_type);
+
 		
 		// userDetail에 있는 정보를 넣을 필드를 초기화 함.
 		// user_id 초기화
@@ -47,9 +52,10 @@ public class ProductsFilterController {
 			user_id = userDetail.getUsername();
 			auth = userDetail.getAuthorities();
 		}
-//		System.out.println("로그인하고 안하고 확인하기 -> "+user_id);
-//		System.out.println("로그인하고 안하고 확인하기 -> "+auth);
 		
+		log.info("로그인하고 안하고 확인하기 user_id ->  {}", user_id);
+		log.info("로그인하고 안하고 확인하기 auth ->  {}", auth);
+
 		// accom 모델에 넣어주기 
 		accom.setAccom_type(accom_type);
 		
@@ -89,7 +95,9 @@ public class ProductsFilterController {
 									   	  @RequestParam("addr") String addr,
 									   	  Accom accom,
 									   	  Model model) {
-		//System.out.println("컨트롤러 cgProductListsByAddr 시작");
+		
+		log.info("컨트롤러 cgProductListsByAddr 시작");
+		
 		// userDetail에 있는 정보를 넣을 필드를 초기화 함.
 		// user_id 초기화
 		String user_id = "";
@@ -105,8 +113,8 @@ public class ProductsFilterController {
 			user_id = userDetail.getUsername();
 			auth = userDetail.getAuthorities();
 		}
-//		System.out.println("로그인하고 안하고 확인하기 -> "+user_id);
-//		System.out.println("로그인하고 안하고 확인하기 -> "+auth);
+		log.info("로그인하고 안하고 확인하기 user_id ->  {}", user_id);
+		log.info("로그인하고 안하고 확인하기 auth ->  {}", auth);
 		
 		// id를 세팅하는 이유는 서비스에 가서 로그인한 아이디 값으로 zzim 목록을 가져오기 위함
 		// 그리고 zzim 리스트의 user_id와 로그인한 아이디 값이 일치했을 때 accom모델에 있는 zzim_status에 Y라는 값을 넣기 위함. 서비스 딴 참고.
@@ -116,9 +124,10 @@ public class ProductsFilterController {
 		
 		List<Accom> cgProductListsByAddr = service.cgGetProductListsByAddr(accom);
 		
-//		System.out.println("컨트롤러 cgProductListsByAddr getAccom_type, addr-> "+accom.getAccom_type() +"," + accom.getAddr());
-//		System.out.println("컨트롤러 cgProductListsByAddr 사이즈 -> "+cgProductListsByAddr.size());
-		
+		log.info("컨트롤러 cgProductListsByAddr getAccom_type -> {} ", accom.getAccom_type());
+		log.info("컨트롤러 cgProductListsByAddr addr-> {} ", accom.getAddr());
+		log.info("컨트롤러 cgProductListsByAddr 사이즈-> {} ", cgProductListsByAddr.size());
+				
 		// accom_type이 호텔들이면 호텔페이지로 넘어가게 했음.
 		// 페이지를 나누는 이유는 ajax 작동방식이 다르고 필터바 내용도 다르기 때문
 		if(accom_type.equals("511,513,514,515")) {
